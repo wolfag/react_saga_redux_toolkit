04385943fa497a849e2eb0a47fc7d4e0bfd34833
TaiNguyen <tai@useget.com>
2021-07-11 14:43:50 +0700

    use call for recommend to test

--- src/features/counter/counterSaga.ts
+++ src/features/counter/counterSaga.ts
@@ -1,6 +1,8 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { delay, put, takeEvery, takeLatest } from 'redux-saga/effects';
+import { call, put, takeEvery, takeLatest } from 'redux-saga/effects';
 import { incrementSaga, incrementSagaSuccess } from './counterSlice';
+
+export const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
 export function* log(action: PayloadAction) {
   console.log('log:', action);
@@ -9,7 +11,7 @@ export function* log(action: PayloadAction) {
 function* handleInc(action: PayloadAction<number>) {
   console.log('handleInc waiting');
 
-  yield delay(2000);
+  yield call(delay, 1000);
 
   console.log('handleInc waiting done');
 
