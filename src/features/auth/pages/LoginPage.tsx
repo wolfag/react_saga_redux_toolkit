6d4ec6d4f6710d0aed4a9a9c01bb58b1e08f01ca
TaiNguyen <tai@useget.com>
2021-07-17 11:38:22 +0700

    material ui

--- src/features/auth/pages/LoginPage.tsx
+++ src/features/auth/pages/LoginPage.tsx
@@ -1,7 +1,35 @@
+import { Paper, makeStyles, Typography, Box, Button } from '@material-ui/core';
 import React, { ReactElement } from 'react';
 
 interface Props {}
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'wrap',
+    justifyContent: 'center',
+    alignItems: 'center',
+    minHeight: '100vh',
+  },
+  box: {
+    padding: theme.spacing(3),
+  },
+}));
+
 export default function LoginPage({}: Props): ReactElement {
-  return <div>Login page</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Paper elevation={1} className={classes.box}>
+        <Typography variant="h5" component="h1">
+          Student Management
+        </Typography>
+        <Box mt={4}>
+          <Button fullWidth variant="contained" color="primary">
+            Fake Login
+          </Button>
+        </Box>
+      </Paper>
+    </div>
+  );
 }
